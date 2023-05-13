08a2431085adc33b80b965b6d86a6e4c474adc8a
Akash Gupta <akash.gupta@nw18.com>
2023-05-13 14:36:02 +0530

    added comments

--- src/main/java/design_pattern/singleton_138/Singleton.java
+++ src/main/java/design_pattern/singleton_138/Singleton.java
@@ -11,3 +11,6 @@ public class Singleton {
         return instance;
     }
 }
+/* problems with this structure
+1. We are doing Eager initialization which makes application startup time slow
+2. Application might create multiple objects in multithreaded environment, etc */
